05f6319bdbfd39feb17045ea23f1f5b6a3241d17
Fritz Anderson <fritza@uchicago.edu>
2019-02-17 11:58:34 -0600

    Initial Commit

---
+++ Simple CPU/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Simple CPU
+//
+//  Created by Fritz Anderson on 2/17/19.
+//  Copyright © 2019 Fritz Anderson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Simple CPUTests/Simple_CPUTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simple_CPUTests.swift
+//  Simple CPUTests
+//
+//  Created by Fritz Anderson on 2/17/19.
+//  Copyright © 2019 Fritz Anderson. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_CPU
+
+class Simple_CPUTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
